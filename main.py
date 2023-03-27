1dce16a854e3139c54d6b1929c849a2e2d386561
robertfenechadami <robertfa02@gmail.com>
2023-03-27 12:46:15 +0200

    Updated

--- main.py
+++ main.py
@@ -1,49 +1,55 @@
-import random
-from flask import Flask, jsonify
-from sqlalchemy import create_engine, Column, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from flask import Flask, jsonify, request
+from google.cloud import sqlconnector
 
 app = Flask(__name__)
 
-
-# Set up SQLAlchemy engine and session to connect to Cloud SQL database
-db_user = 'rob'
-db_password = 'stfxv6gvwD+mxP+8JoKnfw2g'
-db_name = 'random-numbers'
-db_instance_name = 'db-instance'
-cloud_sql_connection_name = f'{PROJECT_NAME}:{REGION}:{db_instance_name}'
-engine = create_engine(
-    f'mysql+pymysql://{db_user}:{db_password}@localhost/{db_name}?unix_socket=/cloudsql/{cloud_sql_connection_name}'
+# Initialize the SQL connection pool
+db_user = "rob"
+db_pass = "YFDH7aB9i0f6gNhkDaztTqjQ"
+db_name = "random-numbers"
+db_socket_dir = "/cloudsql"
+cloud_sql_connection_name = "CIS3111-assignment:europe-west1:db-instance"
+db_pool = sqlconnector.connect(
+    user=db_user,
+    password=db_pass,
+    unix_socket=f"{db_socket_dir}/{cloud_sql_connection_name}/{db_name}"
 )
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
 
-
-# Define ORM model for random numbers table
-class RandomNumber(Base):
-    __tablename__ = 'random_numbers'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    value = Column(Integer)
-
-# Generate 1,000 random integers between 0 and 100,000 and store them in the database
+# API endpoint for generating and storing random numbers
+@app.route("/generate", methods=["POST"])
 def generate_random_numbers():
-    numbers = [RandomNumber(value=random.randint(0, 100000)) for _ in range(1000)]
-    with Session() as session:
-        session.add_all(numbers)
-        session.commit()
+    # Get the number of random numbers to generate from the request
+    num_numbers = request.json.get("num_numbers", 1000)
+    
+    # Generate the random numbers
+    random_numbers = [random.randint(0, 100000) for i in range(num_numbers)]
+    
+    # Store the random numbers in the database
+    with db_pool.cursor() as cursor:
+        insert_query = "INSERT INTO random_numbers (number) VALUES (%s)"
+        values = [(num,) for num in random_numbers]
+        cursor.executemany(insert_query, values)
+        db_pool.commit()
+    
+    # Return a success message
+    return jsonify({"message": f"Generated and stored {num_numbers} random numbers"})
 
-# API endpoint to trigger random number generation and return min/max values
-@app.route('/random_numbers')
-def get_random_numbers():
-    generate_random_numbers()
-    with Session() as session:
-        numbers = [n.value for n in session.query(RandomNumber).all()]
+
+# API endpoint for retrieving the largest and smallest random numbers
+@app.route("/results", methods=["GET"])
+def get_random_number_results():
+    # Retrieve the largest and smallest random numbers from the database
+    with db_pool.cursor() as cursor:
+        select_query = "SELECT MAX(number), MIN(number) FROM random_numbers"
+        cursor.execute(select_query)
+        results = cursor.fetchone()
+    
+    # Format the results as a JSON object and return it
     return jsonify({
-        'min': min(numbers),
-        'max': max(numbers)
+        "largest": results[0],
+        "smallest": results[1]
     })
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
 
+if __name__ == "__main__":
+    app.run()
